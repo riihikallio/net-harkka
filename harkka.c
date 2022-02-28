89cdf3cace4ac77a8e88d299750e6956c906f68b
Petri Riihikallio <petri.riihikallio@metis.fi>
2022-02-28 16:17:05 +0200

    aa

--- harkka.c
+++ harkka.c
@@ -13,9 +13,11 @@
 void client(int sockfd)
 {
     char buff[MAX];
-    int len;
-    while((len = read(sockfd, buff, sizeof(buff))) > 0) {
+    int len = 1;
+    while(len > 0) {
+        len = read(sockfd, buff, sizeof(buff));
         write(STDOUT_FILENO, buff, len);
+        printf("Len: %d\n", len);
     }
 }
    
@@ -26,7 +28,7 @@ int main()
    
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) ERR("socket creation failed");
+    if (sockfd < 0) ERR("socket creation failed");
    
     // get server IP, PORT
     memset(&hints, 0, sizeof(struct addrinfo));
@@ -34,7 +36,7 @@ int main()
     if (getaddrinfo("whx-10.cs.helsinki.fi", "UNIX_TL", &hints, &server) != 0) ERR("getaddrinfo failed");
    
     // connect the client socket to server socket
-    if (connect(sockfd, (SA*)server, sizeof(*server)) != 0) ERR("connection with the server failed");
+    if (connect(sockfd, (SA*)server, sizeof(*server)) < 0) ERR("connection with the server failed");
    
     // do the work
     client(sockfd);
